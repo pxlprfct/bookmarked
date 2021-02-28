d33f092cd50854607bc7139d30f85de958836a9f
Ollie Williams <Oliver.Jack.Williams@googlemail.com>
2021-02-28 20:54:28 +0000

    chore: simplified formatters (#8)
    Co-authored-by: Ollie Williams <Oliver.jack.williams@gmail.com>

--- src/formatters/properties.ts
+++ src/formatters/properties.ts
@@ -1,5 +1,15 @@
+import { hasProperties } from './../utils';
+import { FolderProperties, BookmarkProperties } from './../ts/types';
+
 const format = ([key, value]: [string, unknown]): string => `${key}="${value}"`;
 
-export const formatProperties = (properties: Record<string, unknown>): string =>
+const formatProperties = (properties: Record<string, unknown>): string =>
   // adding a space to the start  " <H3 foo.../>"
   ' ' + Object.entries(properties).map(format).join(' ');
+
+export const addProperties = (
+  properties?: FolderProperties | BookmarkProperties,
+): string =>
+  hasProperties(properties)
+    ? formatProperties(properties as FolderProperties | BookmarkProperties)
+    : '';
