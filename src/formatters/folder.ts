0cda9854422d40e3d4fa6e98b8aaa50bab64f62f
Ollie Williams <Oliver.jack.williams@gmail.com>
2021-02-28 01:39:16 +0000

    feat: updated export

--- src/formatters/folder.ts
+++ src/formatters/folder.ts
@@ -1,6 +1,6 @@
 import { FolderProperties } from '../ts/types';
 import { hasProperties } from './../utils';
-import { buildHtml } from '../index';
+import { buildHtml } from '../buildHtml';
 import { DEFAULT_AMOUNT_TO_PAD_BY } from '../constants';
 import { formatProperties } from './properties';
 import { Folder } from '../ts/types';
