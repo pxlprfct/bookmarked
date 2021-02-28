6b0f55523d34c913d681aeabdf9421320e49ce7e
Ollie Williams <Oliver.jack.williams@gmail.com>
2021-02-28 01:15:39 +0000

    1.1.1

---
+++ src/buildHtml.ts
@@ -0,0 +1,22 @@
+import { addBookmark } from './formatters/bookmark';
+import { addFolder } from './formatters/folder';
+import { indentLeft, isBookmark, isFolder } from './utils';
+import { Bookmark, Folder } from './ts/types';
+
+export const buildHtml = (
+  content: (Bookmark | Folder)[],
+  indent: number,
+): string =>
+  content
+    .reduce((html: string[], current) => {
+      if (isFolder(current)) {
+        html.push(addFolder(current, indentLeft(indent)));
+      }
+
+      if (isBookmark(current)) {
+        html.push(addBookmark(current, indentLeft(indent)));
+      }
+
+      return html;
+    }, [])
+    .join('\n');
