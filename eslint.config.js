f8409a48e57ac8aa6758a7607112b0c7feec0102
ollie w <3811773+pxlprfct@users.noreply.github.com>
2024-09-08 23:17:43 +0100

    chore: readded eslint

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  { ignores: ["dist"] }
+];
